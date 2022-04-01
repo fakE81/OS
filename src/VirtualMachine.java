9e7d6b50f50e34fc47059a0cb8d89ca898e5c43d
fakE81 <62438392+fakE81@users.noreply.github.com>
2022-04-01 19:40:10 +0300

    Update VirtualMachine.java

--- src/VirtualMachine.java
+++ src/VirtualMachine.java
@@ -1,5 +1,12 @@
 public class VirtualMachine {
     
+
+
+    https://github.com/IgnasJ/OS
+    https://github.com/dovius/OS
+    
+
+    // Registrai esantys VM yra susynchronized su RM registrais
     VirtualMachine(){
         // blabla
     
