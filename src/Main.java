ed17e58364d896dc157d750c666049a15d48019a
Lukas Kisevicius <lukas.kisevicius@gmail.com>
2022-04-01 19:39:06 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Starting class.
-        System.out.println(":)");
+        System.out.println(":) BANDAU PAKEISTI ");
     
     }
    }
