6fd8eaddb04261f90c963211e2d0929b819be40b
fakE81 <62438392+fakE81@users.noreply.github.com>
2022-04-02 13:59:45 +0300

    Word added

---
+++ src/Memory/Word.java
@@ -0,0 +1,22 @@
+package Memory;
+
+// Zodis, tik nezinau ar 4B geriau ar 8B
+public class Word {
+    private String data;
+
+
+    Word(){
+        data = "____";
+    }
+    Word(String s){
+        data = s;
+    }
+
+    public String getValue(){
+        return data;
+    }
+    public void setValue(String s){
+        data = s;
+    }
+    // Seteriai geteriai kai paduodamas int.
+}
