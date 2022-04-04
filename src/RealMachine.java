cb043ca64ed9bc63a678b0918052ed7abb396ee0
fakE81 <62438392+fakE81@users.noreply.github.com>
2022-04-04 19:06:21 +0300

    Registers added.

--- src/RealMachine.java
+++ src/RealMachine.java
@@ -7,14 +7,21 @@ import Memory.RealMemory;
 
 public class RealMachine {
     
+    // Registrai:
+    public static int PTR;
+    public static int R0;
+    public static int R1;
 
+    public static byte PC;
+    public static byte SI;
+    public static byte PI;
+    public static byte TI;
+    public static byte mode;
+
+    public static byte[] sf = {0,0,0,0}; // 0-CF,1-ZF,2-SF,3-OF 
 
     private HDD hdd;
     private RealMemory memory;
-    // 1) Registrai.
-    // 2)
-
-
 
     RealMachine(){
         this.hdd = new HDD();
@@ -25,15 +32,14 @@ public class RealMachine {
     public void run(){
         try {
             // Viskas vyksta cia.
-            LoadProgram("Test1.txt");
-            System.out.println(hdd.read(20, 0));
-            System.out.println("Programa sekmingai nuskaityta");
-            memory.setWord("lala", 0, 0);
-            memory.setWord("FFFF", 1, 0);
-            memory.setWord("baba", 2, 0);
+            LoadProgram("Test1.txt"); // Uzsikraunam programa i HDD.
+            VirtualMachine vm = new VirtualMachine(); // Virtuali masina uzkraunama
+            //TODO: Puslapiavimas.
+            vm.run();
+            
+            System.out.println("Realios masinos atmintis:");
             memory.display();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
@@ -45,16 +51,13 @@ public class RealMachine {
                 int r;
                 String programCode= ""; // Tuscia.
                 while((r = reader.read()) != -1){
-                    // Panaikinam newLine ir t.t.
-                    if(r > 30 && r < 150)
+                    // Panaikinam newLine, tarpus.
+                    if(r > 33 && r < 150)
                         programCode += (char) r;
                 }
-                hdd.write(programCode.toCharArray(), 0);
+                HDD.write(programCode.toCharArray(), 0);
             }
     }
-
-
-
 
     
 }
